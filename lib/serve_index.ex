577ad7ae172d85901561980257fb517fe96788b1
Chris Jordan <cjordan@8thlight.com>
2016-11-18 14:10:52 +0000

    Adds react router.

---
+++ lib/serve_index.ex
@@ -0,0 +1,26 @@
+defmodule Perty.ServeIndex do
+  @behaviour Plug
+  import Plug.Conn
+
+  def init(options) do
+    options
+  end
+
+  def call(conn, _opts) do
+    respond(conn.request_path, conn)
+  end
+
+  defp respond("/api/" <> _, conn) do
+    conn
+  end
+
+  defp respond("/emails" <> _, conn) do
+    conn
+  end
+
+  defp respond(_, conn) do
+    conn
+    |> send_file(200, Path.expand("../priv/static/index.html", __DIR__))
+    |> halt
+  end
+end
